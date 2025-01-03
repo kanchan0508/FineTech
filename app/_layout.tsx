504c3e575366bac40d15adb93acc15a29537b00b
kanchan gobari <gobarikanchan53@gmail.com>
2025-01-03 22:08:18 +0530

    Solved error

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,6 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/components/useColorScheme';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -41,7 +40,7 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+ 
 
   return (
   
